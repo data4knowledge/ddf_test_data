1790dd84e2a1baf249f9677ef1f96c358d194362
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-05-18 16:53:42 +0200

    Getting layout sorted

--- app.R
+++ app.R
@@ -2,10 +2,31 @@ library(shiny)
 library(httr)
 
 ui <- fluidPage(
-  actionButton("action", "Request"),
-  textOutput("version"),
   
-  textInput("studyTitle", "Study Title", "Study title ...")
+  titlePanel("DDF Test Data Generator (v0.1)"),
+  sidebarLayout(
+    
+    sidebarPanel(
+      #actionButton("action", "Request"),
+      h4(textOutput("version"))
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Study & Protocol", 
+                 fluidRow(width=12,
+                          column(12,verbatimTextOutput("study"))
+                 ),
+                 fluidRow(width=12,
+                          column(12,textInput("studyTitle", "Study Title", "Study title ...")),
+                 )
+        ), 
+        tabPanel("SoA", verbatimTextOutput("soa")), 
+        tabPanel("Other ...", verbatimTextOutput("other"))
+      )
+    )
+  )
+  
   
 )
 
@@ -15,8 +36,18 @@ server <- function(input, output) {
   
   output$version <- renderText({
     r <- GET("https://byrikz.deta.dev/")
-    paste(toString(content(r)["System"]))
+    if (status_code(r) == 200) {
+      version <- paste0("Using microservice version: ", toString(content(r)["Version"]))
+    } else {
+      version <- "Using microservice version: Not running, error!"
+    }
+    paste(version)
   })
+  
+  output$study <- renderText("We will put data entry for the Study level items here...")
+  output$soa <- renderText("We will put data entry for the SoA items here...")
+  output$other <- renderText("We will put all the other items here...")
+  
 }
 
 shinyApp(ui = ui, server = server)
