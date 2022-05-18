41741478374fbcf8b8736423dd5079b51a38b6ff
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-05-18 18:44:46 +0200

    Get initial post working

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(httr)
+library(jsonlite)
 
 ui <- fluidPage(
   
@@ -15,24 +16,61 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel("Study & Protocol", 
                  fluidRow(width=12,
-                          column(12,verbatimTextOutput("study"))
+                          column(12, h4("Enter Data"))
                  ),
                  fluidRow(width=12,
-                          column(12,textInput("studyTitle", "Study Title", "Study title ...")),
-                 )
+                          column(4, textInput("studyTitle", "Title:", placeholder = "Study title, free text ...")),
+                          column(4, textInput("studyVersion", "Version:", placeholder = "Study version, free text ...")),
+                          column(4, textInput("studyStatus", "Status:", placeholder = "Study status, free text ..."))
+                 ),
+                 fluidRow(width=12,
+                          column(4, selectInput("studyType", "Type:", c("CROSSOVER" = "C12345","PLAIN" = "C12346"))),
+                          column(4, selectInput("studyPhase", "Phase:",
+                                              c("PHASE I" = "C12345",
+                                                "PHASE II" = "C12346",
+                                                "PHASE III" = "C12347")))
+                         ),
+
+                # Not Used                 
+                # study_protocol_version: str
+                 
+                # To be Done
+                # study_identifier: Union[List[StudyIdentifier], List[str], None] = []
+                # study_protocol_reference: Union[StudyProtocol, str, None] = None
+                 
+                fluidRow(width=12,
+                         column(12, h4("Resulting JSON"))
+                ),
+                fluidRow(width=12,
+                         column(12, verbatimTextOutput("jsonview"))),
+
+                fluidRow(width=12,
+                         column(12, h4("Save"))
+                ),
+                fluidRow(width=12,
+                         column(4, actionButton("action", "Submit")),
+                         column(4, textOutput("result"))
+                ),
+                
         ), 
         tabPanel("SoA", verbatimTextOutput("soa")), 
         tabPanel("Other ...", verbatimTextOutput("other"))
       )
     )
   )
-  
-  
 )
 
 server <- function(input, output) {
 
-  observeEvent(input$action, {})
+  observeEvent(input$action, {
+    r <- POST("https://byrikz.deta.dev/study/", body = json_data())
+    if (status_code(r) == 200) {
+      result <- paste0("Using microservice version: ", toString(content(r)["Version"]))
+    } else {
+      result <- "Using microservice version: Not running, error!"
+    }
+    paste(result)
+  })
   
   output$version <- renderText({
     r <- GET("https://byrikz.deta.dev/")
@@ -48,6 +86,20 @@ server <- function(input, output) {
   output$soa <- renderText("We will put data entry for the SoA items here...")
   output$other <- renderText("We will put all the other items here...")
   
+  json_data <- reactive({
+    study_title=(input$studyTitle)
+    study_version=(input$studyVersion)
+    study_status = (input$studyStatus)
+    study_type = (input$studyType)
+    study_phase = (input$studyPhase)
+    study_protocol_version = ("")
+    df <- data.frame(study_title, study_version, study_status, study_type, study_phase, study_protocol_version)
+    toJSON(unbox(df), pretty = TRUE)
+  })
+
+    output$jsonview <- renderPrint({
+    req(json_data())
+  })
 }
 
 shinyApp(ui = ui, server = server)
