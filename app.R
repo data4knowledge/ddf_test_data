c788ef2cd43e1bac364a6e8aeda516b29e6dfb3e
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-05-19 16:41:50 +0200

    Adding in initial study identifiers and amendments

--- app.R
+++ app.R
@@ -1,6 +1,18 @@
 library(shiny)
 library(httr)
 library(jsonlite)
+library(rhandsontable)
+
+d <- c("AAA","BBB","CCC","DDD")
+e <- c("red", "white", "red", "Something")
+f <- c("Code 1","Code 1","Code 1","Code 1")
+mydata <- data.frame(d,e,f)
+names(mydata) <- c("Name","Description","Org Code") 
+
+g <- c("AAA","BBB","CCC","DDD")
+h <- c("red", "white", "red", "Something")
+amendmentdata <- data.frame(g, h)
+names(amendmentdata) <- c("Effective Date","Version") 
 
 ui <- fluidPage(
   
@@ -16,42 +28,54 @@ ui <- fluidPage(
     mainPanel(
       tabsetPanel(
         tabPanel("Study & Protocol", 
-                 fluidRow(width=12,
-                          column(12, h4("Enter Data"))
-                 ),
-                 fluidRow(width=12,
-                          column(4, textInput("studyTitle", "Title:", placeholder = "Study title, free text ...")),
-                          column(4, textInput("studyVersion", "Version:", placeholder = "Study version, free text ...")),
-                          column(4, textInput("studyStatus", "Status:", placeholder = "Study status, free text ..."))
-                 ),
-                 fluidRow(width=12,
-                          column(4, selectInput("studyType", "Type:", c("CROSSOVER" = "C12345","PLAIN" = "C12346"))),
-                          column(4, selectInput("studyPhase", "Phase:",
-                                              c("PHASE I" = "C12345",
-                                                "PHASE II" = "C12346",
-                                                "PHASE III" = "C12347")))
+                 wellPanel(
+                   fluidRow(width=12,
+                            column(12, h4("Basic Data"))
+                   ),
+                   fluidRow(width=12,
+                            column(4, textInput("studyTitle", "Title:", placeholder = "Study title, free text ...")),
+                            column(4, textInput("studyVersion", "Version:", placeholder = "Study version, free text ...")),
+                            column(4, textInput("studyStatus", "Status:", placeholder = "Study status, free text ..."))
+                   ),
+                   fluidRow(width=12,
+                            column(4, selectInput("studyType", "Type:", c("CROSSOVER" = "C12345","PLAIN" = "C12346"))),
+                            column(4, selectInput("studyPhase", "Phase:",
+                                                c("PHASE I" = "C12345",
+                                                  "PHASE II" = "C12346",
+                                                  "PHASE III" = "C12347")))
+                           )
+                ),
+                
+                # study_identifier: Union[List[StudyIdentifier], List[str], None] = []
+                fluidRow(width=12, 
+                         column ( 6, 
+                                  wellPanel(
+                                    h4("Study Identifiers"),
+                                    rHandsontableOutput("hottable"),
+                                    actionButton("save1", "Save")
+                                  )
                          ),
-
+                         column ( 6, 
+                                  wellPanel(
+                                    h4("Amendments"),
+                                    rHandsontableOutput("amendments"),
+                                    actionButton("save2", "Save")
+                                  )
+                         ),
+                ),
                 # Not Used                 
                 # study_protocol_version: str
                  
                 # To be Done
-                # study_identifier: Union[List[StudyIdentifier], List[str], None] = []
                 # study_protocol_reference: Union[StudyProtocol, str, None] = None
                  
-                fluidRow(width=12,
-                         column(12, h4("Resulting JSON"))
-                ),
-                fluidRow(width=12,
-                         column(12, verbatimTextOutput("jsonview"))),
-
-                fluidRow(width=12,
-                         column(12, h4("Save"))
-                ),
+                fluidRow(width=12, column(12, h4("Resulting JSON"))),
+                fluidRow(width=12, column(12, verbatimTextOutput("jsonview"))),
+                fluidRow(width=12, column(12, h4("Save"))),
                 fluidRow(width=12,
                          column(4, actionButton("action", "Submit")),
                          column(4, textOutput("result"))
-                ),
+                )
                 
         ), 
         tabPanel("SoA", verbatimTextOutput("soa")), 
@@ -98,8 +122,8 @@ server <- function(input, output) {
   output$other <- renderText("We will put all the other items here...")
   
   json_data <- reactive({
-    study_title=(input$studyTitle)
-    study_version=(input$studyVersion)
+    study_title = (input$studyTitle)
+    study_version = (input$studyVersion)
     study_status = (input$studyStatus)
     study_type = (input$studyType)
     study_phase = (input$studyPhase)
@@ -111,6 +135,20 @@ server <- function(input, output) {
     output$jsonview <- renderPrint({
     req(json_data())
   })
+    
+  output$hottable <- renderRHandsontable({
+    rhandsontable(mydata)
+  })
+  
+  output$amendments <- renderRHandsontable({
+    rhandsontable(amendmentdata)
+  })
+
+  observeEvent(input$save, { 
+    si_df <- hot_to_r(input$rhandsontable)
+    print(si_df)
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
